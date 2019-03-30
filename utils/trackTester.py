302e58497cf13ea8c7fa22b2c898b3c671dfb4ba
ArthurBeaulieu <contact@abeaulieu.fr>
2019-03-30 23:20:05 +0100

    remove useless print

--- utils/trackTester.py
+++ utils/trackTester.py
@@ -135,12 +135,12 @@ class TrackTester:
         if self.track.composers == '':
             self.missingTagsCounter += 1
             self.missingTags.append('Composers')
-        # if self.track.producer == '':
-        #     self.missingTagsCounter += 1
-        #     self.missingTags.append('Producer')
-        # if self.track.label == '':
-        #     self.missingTagsCounter += 1
-        #     self.missingTags.append('Label')
+        if self.track.producer == '':
+            self.missingTagsCounter += 1
+            self.missingTags.append('Producer')
+        if self.track.label == '':
+            self.missingTagsCounter += 1
+            self.missingTags.append('Label')
         if self.track.trackNumber == '':
             self.missingTagsCounter += 1
             self.missingTags.append('TrackNumber')
